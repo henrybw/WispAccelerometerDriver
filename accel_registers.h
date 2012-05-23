0ffbdd6bb3baf9c24966ce6c886c87ed147d73f5
Henry Baba-Weiss <henry.babaweiss@gmail.com>
2012-05-23 02:48:19 -0700

    Fixed register map, commented.

--- accel_registers.h
+++ accel_registers.h
@@ -8,38 +8,42 @@
 #ifndef ACCEL_REGISTERS_H_
 #define ACCEL_REGISTERS_H_
 
-#define 0x00 DEVID  // Device ID
-#define 0x1D THRESH_TAP  // Tap threshold 
-#define 0x1E OFSX  // X-axis offset 
-#define 0x1F OFSY  // Y-axis offset 
-#define 0x20 OFSZ  // Z-axis offset 
-#define 0x21 DUR  // Tap duration 
-#define 0x22 Latent  // Tap latency 
-#define 0x23 Window  // Tap window 
-#define 0x24 THRESH_ACT  // Activity threshold 
-#define 0x25 THRESH_INACT  // Inactivity threshold 
-#define 0x26 TIME_INACT  // Inactivity time 
-#define 0x27 ACT_INACT_CTL  // Axis enable control for activity and inactivity detection 
-#define 0x28 THRESH_FF  // Free-fall threshold 
-#define 0x29 TIME_FF  // Free-fall time 
-#define 0x2A TAP_AXES  // Axis control for single tap/double tap 
-#define 0x2B ACT_TAP_STATUS  // Source of single tap/double tap 
-#define 0x2C BW_RATE  // Data rate and power mode control 
-#define 0x2D POWER_CTL  // Power-saving features control 
-#define 0x2E INT_ENABLE  // Interrupt enable control 
-#define 0x2F INT_MAP  // Interrupt mapping control 
-#define 0x30 INT_SOURCE  // Source of interrupts 
-#define 0x31 DATA_FORMAT  // Data format control 
-#define 0x32 DATAX0  // X-Axis Data 0 
-#define 0x33 DATAX1  // X-Axis Data 1 
-#define 0x34 DATAY0  // Y-Axis Data 0 
-#define 0x35 DATAY1  // Y-Axis Data 1 
-#define 0x36 DATAZ0  // Z-Axis Data 0 
-#define 0x37 DATAZ1  // Z-Axis Data 1 
-#define 0x38 FIFO_CTL  // FIFO control 
-#define 0x39 FIFO_STATUS  // FIFO status 
-#define 0x3A TAP_SIGN  // Sign and source for single tap/double tap 
-#define 0x3B ORIENT_CONF  // Orientation configuration 
-#define 0x3C Orient  // Orientation status
+//
+// Created from accel_registers.txt, which was in turn copied and pasted from
+// the register map in the ADXL346 data sheet (table 19, page 22).
+//
+#define REG_DEVID			0x00	// Device ID
+#define REG_THRESH_TAP		0x1D	// Tap threshold 
+#define REG_OFSX			0x1E	// X-axis offset 
+#define REG_OFSY			0x1F	// Y-axis offset 
+#define REG_OFSZ			0x20	// Z-axis offset 
+#define REG_DUR				0x21	// Tap duration 
+#define REG_Latent			0x22	// Tap latency 
+#define REG_Window			0x23	// Tap window 
+#define REG_THRESH_ACT		0x24	// Activity threshold 
+#define REG_THRESH_INACT	0x25	// Inactivity threshold 
+#define REG_TIME_INACT		0x26	// Inactivity time 
+#define REG_ACT_INACT_CTL	0x27	// Axis enable control for activity and inactivity detection 
+#define REG_THRESH_FF		0x28	// Free-fall threshold 
+#define REG_TIME_FF			0x29	// Free-fall time 
+#define REG_TAP_AXES		0x2A	// Axis control for single tap/double tap 
+#define REG_ACT_TAP_STATUS	0x2B	// Source of single tap/double tap 
+#define REG_BW_RATE			0x2C	// Data rate and power mode control 
+#define REG_POWER_CTL		0x2D	// Power-saving features control 
+#define REG_INT_ENABLE		0x2E	// Interrupt enable control 
+#define REG_INT_MAP			0x2F	// Interrupt mapping control 
+#define REG_INT_SOURCE		0x30	// Source of interrupts 
+#define REG_DATA_FORMAT		0x31	// Data format control 
+#define REG_DATAX0			0x32	// X-Axis Data 0 
+#define REG_DATAX1			0x33	// X-Axis Data 1 
+#define REG_DATAY0			0x34	// Y-Axis Data 0 
+#define REG_DATAY1			0x35	// Y-Axis Data 1 
+#define REG_DATAZ0			0x36	// Z-Axis Data 0 
+#define REG_DATAZ1			0x37	// Z-Axis Data 1 
+#define REG_FIFO_CTL		0x38	// FIFO control 
+#define REG_FIFO_STATUS		0x39	// FIFO status 
+#define REG_TAP_SIGN		0x3A	// Sign and source for single tap/double tap 
+#define REG_ORIENT_CONF		0x3B	// Orientation configuration 
+#define REG_Orient			0x3C	// Orientation status
 
 #endif /* ACCEL_REGISTERS_H_ */
